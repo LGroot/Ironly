b41ef5cad8e22058b1bfd0b2b752a76e8cbec420
Laurens Groot <groot@scm.com>
2023-09-07 23:51:08 +0200

    Initial bot commit. Retrieves nicknames and ranks with '/stat' command.

---
+++ bot.py
@@ -0,0 +1,52 @@
+"""A bot to list all members of a server."""
+import csv, os, time
+from dotenv import load_dotenv
+ 
+from discord.ext import commands
+from discord.ext.commands import Bot
+from discord import File, Intents
+ 
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+ 
+intents = Intents.default()
+intents.members = True
+bot = Bot(intents=intents)
+ 
+ 
+@bot.event
+async def on_ready():
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
+ 
+ 
+@bot.event
+async def on_command_error(error, ctx):
+    if isinstance(error, commands.CommandNotFound):
+        return
+    else:
+        print(error)
+ 
+ 
+@bot.slash_command(guild_ids=[1140620898773245952])
+async def stat(ctx):
+    """Returns a CSV file of all users on the server."""
+    await ctx.respond("Check your DMs", ephemeral=True)
+    before = time.time()
+    nicknames = [m.display_name for m in ctx.guild.members]
+    roles = [m.roles for m in ctx.guild.members]
+    
+    with open('temp.csv', mode='w', encoding='utf-8', newline='') as f:
+        writer = csv.writer(f, dialect='excel')
+        for v,role in zip(nicknames, roles):
+            writer.writerow([v, ",".join([r.name for r in role])])
+    after = time.time()
+    dm = await bot.create_dm(ctx.author)
+    await dm.send(file=File('temp.csv', filename='stats.csv'),
+                        content="Here you go! Generated in {:.4}ms.".format((after - before)*1000))
+ 
+ 
+if __name__ == '__main__':
+    bot.run(TOKEN)
